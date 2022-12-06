2ee160fadc4d700ea168a9023f19361fdbcf4698
Anton <rumiant348@yandex.ru>
2022-12-06 11:52:21 +0800

    add delete form

--- models/images.go
+++ models/images.go
@@ -9,7 +9,7 @@ import (
 
 type ImageService interface {
 	Create(galleryID uint, r io.Reader, filename string) error
-	ByGalleryID(galleryID uint) ([]string, error)
+	ByGalleryID(galleryID uint) ([]Image, error)
 }
 
 func NewImageService() ImageService {
@@ -37,17 +37,20 @@ func (is *imageService) Create(galleryID uint, r io.Reader, filename string) err
 	return nil
 }
 
-func (is *imageService) ByGalleryID(galleryID uint) ([]string, error) {
+func (is *imageService) ByGalleryID(galleryID uint) ([]Image, error) {
 	path := is.imageDir(galleryID)
 	strings, err := filepath.Glob(filepath.Join(path, "*"))
 	if err != nil {
 		return nil, err
 	}
-	// Add a leading "/" to all image file paths
-	for i := range strings {
-		strings[i] = "/" + strings[i]
+	ret := make([]Image, len(strings))
+	for i, imgStr := range strings {
+		ret[i] = Image{
+			Filename:  filepath.Base(imgStr),
+			GalleryID: galleryID,
+		}
 	}
-	return strings, err
+	return ret, err
 }
 
 func (is *imageService) mkImageDir(galleryID uint) (string, error) {
@@ -62,4 +65,25 @@ func (is *imageService) mkImageDir(galleryID uint) (string, error) {
 func (is *imageService) imageDir(galleryID uint) string {
 	return filepath.Join("images", "galleries",
 		fmt.Sprintf("%v", galleryID))
+}
+
+// Image is used to represent images stored in a Gallery
+// Image is not stored in the db, and instead
+// references data stored on disk.
+type Image struct {
+	GalleryID uint
+	Filename  string
+}
+
+// Path is used to build the absolute path used to reference this image
+// via a web request.
+func (i *Image) Path() string {
+	return "/" + i.RelativePath()
+}
+
+// RelativePath is used to build the path to this image on our local
+// disk, relative to where our Go application is run from.
+func (i *Image) RelativePath() string {
+	galleryID := fmt.Sprintf("%v", i.GalleryID)
+	return filepath.ToSlash(filepath.Join("images", "galleries", galleryID, i.Filename))
 }
